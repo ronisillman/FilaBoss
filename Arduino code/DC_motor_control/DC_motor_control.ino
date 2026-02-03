9035083a96f1c8f238148bf8233772deb642363c
ronisillman <154622032+ronisillman@users.noreply.github.com>
2026-02-03 16:36:07 +0200

    DC motor control code init

---
+++ Arduino code/DC_motor_control/DC_motor_control.ino
@@ -0,0 +1,35 @@
+const int motorRollerPin = 3;
+const int motorSpoolPin = 11;
+
+int speed_Roller = 30;
+int speed_Spool = 20;
+
+void setup() {
+  pinMode(motorRollerPin, OUTPUT);
+  pinMode(motorSpoolPin, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println("Beginning motor code.");
+
+  // Start motors immediately
+  analogWrite(motorRollerPin, speed_Roller);
+  analogWrite(motorSpoolPin, speed_Spool);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    speed_Roller = Serial.parseInt();
+    speed_Spool  = Serial.parseInt();
+
+    speed_Roller = constrain(speed_Roller, 0, 255);
+    speed_Spool  = constrain(speed_Spool, 0, 255);
+
+    Serial.print("Roller Speed: ");
+    Serial.print(speed_Roller);
+    Serial.print(" | Spool Speed: ");
+    Serial.println(speed_Spool);
+  }
+
+  analogWrite(motorRollerPin, speed_Roller);
+  analogWrite(motorSpoolPin, speed_Spool);
+}
